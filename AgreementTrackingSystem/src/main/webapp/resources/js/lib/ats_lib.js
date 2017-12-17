b9ac3a2117f6a568c3c8f93b21a0f17a8ea1814f
apurbo <jsr143rahman@gmail.com>
2017-12-17 18:19:21 +0600

    Angular js

--- AgreementTrackingSystem/src/main/webapp/resources/js/lib/ats_lib.js
+++ AgreementTrackingSystem/src/main/webapp/resources/js/lib/ats_lib.js
@@ -105,4 +105,56 @@ atsGlobal = (function () {
             }
         });
     }
+
+    function convertAmountInWord(amount) {
+        var th = ['', 'thousand', 'million', 'billion', 'trillion'];
+        var dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+        var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+        var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+        amount = amount.toString().trim();
+        amount = amount.replace(/[\, ]/g, '');
+        if (!amount || amount == 'NaN') {
+            return '';
+        }
+        if (amount != parseFloat(amount)) return 'not a number';
+        var x = amount.indexOf('.');
+        if (x == -1)
+            x = amount.length;
+        if (x > 15)
+            return 'too big';
+        var n = amount.split('');
+        var str = '';
+        var sk = 0;
+        for (var i = 0; i < x; i++) {
+            if ((x - i) % 3 == 2) {
+                if (n[i] == '1') {
+                    str += tn[Number(n[i + 1])] + ' ';
+                    i++;
+                    sk = 1;
+                } else if (n[i] != '0') {
+                    str += tw[n[i] - 2] + ' ';
+                    sk = 1;
+                }
+            } else if (n[i] != '0') { // 0235
+                str += dg[n[i]] + ' ';
+                if ((x - i) % 3 == 0) str += 'Hundred ';
+                sk = 1;
+            }
+            if ((x - i) % 3 == 1) {
+                if (sk)
+                    str += th[(x - i - 1) / 3] + ' ';
+                sk = 0;
+            }
+        }
+
+        if (x != amount.length) {
+            var y = amount.length;
+            str += 'point ';
+            for (var i = x + 1; i < y; i++)
+                str += dg[n[i]] + ' ';
+        }
+        return str.replace(/\s+/g, ' ');
+
+    }
 })();
--- AgreementTrackingSystem/src/main/webapp/resources/js/lib/ats_services.js
+++ AgreementTrackingSystem/src/main/webapp/resources/js/lib/ats_services.js
@@ -10,7 +10,110 @@ app.factory('httpDataIntercept', ['$q', function ($q) {
                 window.location.href = atsGlobal.baseUrl() + "login";
                 break;
             case 500:
-                errorMsg()
+                errorMsg("There is unwanted exception. Please contact with ....");
+                break;
+            case 404:
+                errorMsg("Invalid Request -404");
+                break;
+            case 400:
+                errorMsg("Bad Request -400");
+                break;
+            case 406:
+                window.location.href = atsGlobal.baseUrl() + "your customized message";
+                break;
+            //and so on you can add more if required;
         }
+
     }
+
+    return {
+        request: function (config) {
+            config.headers['X-Requested-With'] = 'XMLHttpRequest';
+            return config;
+        },
+        response: function (data) {
+            checkStatus(data.status);
+            return data;
+        },
+        responseError: function (data) {
+            checkStatus(data.status);
+            return data;
+        }
+    };
 }]);
+
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('httpDataInterceptor');
+}]);
+
+app.service(
+    'atsGlobalService', [function () {
+        this.baseUrl = function () {
+            return window.location.protocol + '//' + window.location.host + '/ats.web';
+        };
+    }]
+);
+
+app.service('formatService', [function () {
+        this.formatFinancialValue = function (data) {
+            if (data) {
+                if (data > 999999999999.9999) {
+                    return '';
+                }
+                data = data.toString();
+                var amountFormat = globalConf.financialValueFormat;
+                var amount = '';
+                var place = globalConf.decimalPlacesForAmount;
+                var divisor = '1' + Array(place).join(0);
+                if (data.indexOf(',') === -1) {
+                    amount = data;
+                } else {
+                    amount = data.split(',').join('');
+                }
+                var roundAmount = Math.floor(Number(amount) * Number(divisor)) / Number(divisor);
+                if (amount < 0) {
+                    roundAmount = Math.ceil(Number(amount) * Number(divisor)) / Number(divisor);
+                }
+                var amountParts = roundAmount.toFixed(place - 1).toString().split('.');
+                var formatRegx = RegExp(amountFormat, 'g');
+                var formatedAmount = '';
+                if (amountFormat.indexOf('(') === -1) {
+                    formatedAmount = amountParts[0] + (amountParts[1] ? '.' + amountFormat[1] : '');
+                } else {
+                    formatedAmount = amountParts[0].replace(formatRegx, "$1,") +
+                    (amountParts[1] ? '.' + amountParts[1] : '');
+                }
+                return formatedAmount;
+            }
+        };
+
+        this.convertAmountInWords = function (data) {
+            if (data) {
+                return atsGlobal.convertAmountInWord(data);
+            }
+        };
+        this.formatGlobalDate = function (data, returnAsDate) {
+            if (data) {
+
+            }
+        }
+    }]
+);
+app.service(
+    'ajaxService', ['$http', 'atsGlobalService', function ($http, atsGlobalService) {
+        this.get = function (url, params) {
+            return $http(
+                {
+                    url: atsGlobalService.baseUrl() + url,
+                    method: 'GET',
+                    params: params
+                }
+            );
+        };
+
+        this.post = function (url, data) {
+            var token = $('input[name="_csrf"]').va();
+            return $http.pos(atsGlobalService.baseUrl() + url, data, {headers: {'X-CSRF-TOKEN': token}});
+        };
+    }]
+);
